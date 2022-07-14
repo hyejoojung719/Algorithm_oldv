be35472b717d71d19a820b098a116eb94b476816
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-07-14 15:16:26 +0900

    keep....

--- Test/src/Algo/Main_Jungol_127반복제어문1_형성평가3.java
+++ Test/src/Algo/Main_Jungol_127반복제어문1_형성평가3.java
@@ -3,7 +3,7 @@ package Algo;
 import java.util.Scanner;
 
 public class Main_Jungol_127반복제어문1_형성평가3 {
-	// 보류..
+	// 보류.. 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
