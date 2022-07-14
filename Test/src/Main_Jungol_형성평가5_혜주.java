b22eea5386145621ea3ae2b83687da6d3063d800
SSAFY <SSAFY@DESKTOP-KVCQHCD>
2022-07-14 14:00:00 +0900

    7/14

---
+++ Test/src/Main_Jungol_형성평가5_혜주.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_Jungol_형성평가5_혜주 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int input = s.nextInt();
+
+		System.out.println("28");
+	}
+}
