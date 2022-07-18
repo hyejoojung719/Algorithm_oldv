04cecf9a7bae0f5063fb4837ea31c15a4b7c90a0
DESKTOP-9ODL4PS\fixup <fixup719@gmail.com>
2022-07-18 15:43:41 +0900

    7/18

--- Test/src/swea/Main_Swea_2072홀수만더하기.java
+++ Test/src/swea/Main_Swea_2072홀수만더하기.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_Swea_2072È¦¼ö¸¸´õÇÏ±â {
+public class Main_Swea_2072í™€ìˆ˜ë§Œë”í•˜ê¸°{
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
