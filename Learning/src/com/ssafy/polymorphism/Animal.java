1295ad710fbc0cbc76199217cb8f0fe6d43a6fb8
hyejoojung <fixup719@gmail.com>
2022-07-22 11:14:48 +0900

    1114

--- Learning/src/com/ssafy/polymorphism/Animal.java
+++ Learning/src/com/ssafy/polymorphism/Animal.java
@@ -1,11 +1,17 @@
 package com.ssafy.polymorphism;
 
 public class Animal {
+	
+	String type;
+	
 
 	public void eat() {
 		System.out.println("Animal의 eat()");
 	}
 	
+	public void animal() {
+		System.out.println("Animal의 animal()");
+	}
 	
 	
 	
---
+++ Learning/src/com/ssafy/singleton/Company.java
@@ -0,0 +1,5 @@
+package com.ssafy.singleton;
+
+public class Company {
+
+}
