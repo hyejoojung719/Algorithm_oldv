e04a25bbd381912b1057adc05878ac012f31e72c
hyejoojung <fixup719@gmail.com>
2022-07-27 08:37:16 +0900

    0837

--- Algorithm/src/boj/Main_Boj_9935문자열폭파.java
+++ Algorithm/src/boj/Main_Boj_9935문자열폭파.java
@@ -1,9 +1,29 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main_Boj_9935문자열폭파 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String bbang = br.readLine();
+		
+		for(int i=0; i<str.length(); i++) {
+			for(int j=0; j<bbang.length(); j++) {
+				if(str.charAt(i)==bbang.charAt(j))
+			}
+		}
+		
+		
+		
+		br.close();
+		
 
 	}
 
