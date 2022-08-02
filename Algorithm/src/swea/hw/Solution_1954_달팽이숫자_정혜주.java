b1a723e86e3c4a4e85c03da0958e611f7cb357b1
정혜주 <fixup719@gmail.com>
2022-08-03 08:35:08 +0900

    08030835

--- Algorithm/src/swea/hw/Solution_1954_달팽이숫자_정혜주.java
+++ Algorithm/src/swea/hw/Solution_1954_달팽이숫자_정혜주.java
@@ -8,7 +8,7 @@ public class Solution_1954_달팽이숫자_정혜주 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int test_case = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<test_case; tc++) {
+		for(int tc=1; tc<=test_case; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int[][] snail = new int[N][N];
 			
@@ -20,35 +20,49 @@ public class Solution_1954_달팽이숫자_정혜주 {
 			int x = 0;
 			int y = 0;
 			int num = 1;
-			while(x>=0 && x<N && y>=0 && y<N) {
-				if(snail[x][y]==0) snail[x][y]=num;
-				else break;
-				y++;
-				num++;
-			}
-			x++;
-			// (x,y) = (0,2)
-			while(x>=0 && x<N && y>=0 && y<N) {
-				if(snail[x][y]==0) snail[x][y]=num;
-				else break;
+		
+			for(int i=0; i<N/2+1; i++) {
+				while(x>=0 && x<N && y>=0 && y<N) {
+					if(snail[x][y]==0) snail[x][y]=num;
+					else break;
+					y++;
+					num++;
+				}
+				
 				x++;
-				num++;
-			}
-			y--;
-			// (x,y) = (2,2)
-			while(x>=0 && x<N && y>=0 && y<N) {
-				if(snail[x][y]==0) snail[x][y]=num;
-				else break;
+				// (x,y) = (0,2)
+				while(x>=0 && x<N && y>=0 && y<N) {
+					if(snail[x][y]==0) snail[x][y]=num;
+					else break;
+					x++;
+					num++;
+				}
+				
 				y--;
-				num++;
-			}
-			x--;
-			// (x,y) = (2,0)
-			while(x>=0 && x<N && y>=0 && y<N) {
-				if(snail[x][y]==0) snail[x][y]=num;
-				else break;
+				// (x,y) = (2,2)
+				while(x>=0 && x<N && y>=0 && y<N) {
+					if(snail[x][y]==0) snail[x][y]=num;
+					else break;
+					y--;
+					num++;
+				}
 				x--;
-				num++;
+				// (x,y) = (2,0)
+				while(x>=0 && x<N && y>=0 && y<N) {
+					if(snail[x][y]==0) snail[x][y]=num;
+					else break;
+					x--;
+					num++;
+				}
+			}
+			
+			
+			System.out.println("#"+tc);
+			for(int[] i : snail) {
+				for(int j : i) {
+					System.out.print(j + " ");
+				}
+				System.out.println();
 			}
 		}
 	}
