4c900f485e58c5cede350bf4532c35cb45f4b5bc
정혜주 <fixup719@gmail.com>
2022-08-12 17:51:45 +0900

    오늘 푼거

--- Algorithm/src/boj/basicAlgoritm/Main_Bj_15686_치킨배달_정혜주.java
+++ Algorithm/src/boj/basicAlgoritm/Main_Bj_15686_치킨배달_정혜주.java
@@ -3,6 +3,10 @@ package boj.basicAlgoritm;
 public class Main_Bj_15686_치킨배달_정혜주 {
 	public static void main(String[] args) {
 		
-		// 
+		// 치킨 거리  : 집과 가장 가까운 치킨집 사이 거리
+		
+		// 도시의 치킨 거리 : 모든 집의 치킨 거리 합
+		
+		// 폐업 시키지 않을 치킨집 최대 M개를 고르고, 도시의 치킨 거리가 가장 작은 경우 구하기
 	}
 }
--- Algorithm/src/swea/hw/Solution_1233_사칙연산유효성검사_정혜주.java
+++ Algorithm/src/swea/hw/Solution_1233_사칙연산유효성검사_정혜주.java
@@ -8,6 +8,17 @@ import java.util.StringTokenizer;
 public class Solution_1233_사칙연산유효성검사_정혜주 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
+		// 1. 노드 탐색(dfs or bfs)
+		// 2. 해당 노드의 유효성을 검사
+				// - 해당 노드가 숫자인 경우
+					// 자식 노드가 없어야 함
+				// - 해당 노드가 연산자인 경우
+					// 자식노드가 2개
+					// 올 수 있는 조합 (1) 둘 다 숫자 (2) 왼쪽 연산자, 오른쪽 숫자인 경우 (3) 둘다 연산자
+		// 이진트리의 특성을 봤을 때 말단은 모두 숫자, 나머지는 연산자여야 함
+			// 인덱스 번호를 고려해서 예를들어 노드가 9개일때 2로 나눴을 때 5개는 말단
+		
+		
 		// 1. 부모 노드가 연산자인 경우
 			// - 좌, 우 자식 노드 유효성 체크
 		// 2. 노드가 숫자 -> 자식노드 없을때만 유효
